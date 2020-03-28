653e0e741e8e471b54c06452d27113e2c3dc6b54
Laura Silvanaviciute <p998ljj@fspa.myntet.se>
2020-03-28 15:28:43 +0200

    feat: added initial locations filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
+import { Place } from './models/place';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +18,26 @@ export class AppComponent implements OnInit {
   // TODO: create separate service for all endpoints
   loadData() {
     return this.http.get('https://www.google.com/maps/timeline/_rpc/ma?pb=').subscribe(data => {
-      console.log(data);
+      console.log('data is fetched');
+      this.filterData(data);
     });
+  }
+
+  filterData(data) {
+    const interestLocations: Place[] = new Array();
+    const weeksBack = 3;	// In weeks
+    const minLocDuration = 10 * 60; // In minutes
+    const timeFromLocations = moment().subtract(weeksBack, 'week').valueOf();
+
+    for (const loc of data[0][0]) {
+      const place = new Place(loc[0], loc[13], loc[1][2], loc[1][3]);
+      if (place.timeFrom < timeFromLocations) { continue; }
+      if (place.timeTo - place.timeFrom < minLocDuration * 1000) { continue; }
+      interestLocations.push(place);
+    }
+
+    for (const loc of interestLocations) {
+      console.log(loc);
+    }
   }
 }
---
+++ src/app/models/place.ts
@@ -0,0 +1,8 @@
+export class Place {
+    constructor(
+        public timeFrom: number,
+        public timeTo: number,
+        public latitude: number,
+        public longitude: number
+    ) { }
+}
