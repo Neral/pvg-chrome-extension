fa43a280d564e6bab5df4ff48a1902e444a1048e
Laura Silvanaviciute <p998ljj@fspa.myntet.se>
2020-03-28 14:21:31 +0200

    feat: extracted initial location data on extention opening

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pvg-chrome-extension';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  // TODO: create separate service for all endpoints
+  loadData() {
+    return this.http.get('https://www.google.com/maps/timeline/_rpc/ma?pb=').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
